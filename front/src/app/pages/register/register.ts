91334dc4dc9fd0e8dc78a9e521fd3bbb63a10a16
Loïc Sadou <loicsadou@Host-003.lan>
2026-02-27 18:13:20 +0100

    [front] component register fait

--- front/src/app/pages/register/register.ts
+++ front/src/app/pages/register/register.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.scss'],
 })
 export class Register {
+
+  username: string = "";
+  email: string = "";
+  password: string = "";
+  errorMsg: string = "";
+
+  constructor(private authService: AuthService, private router: Router){}
+
+  onRegister(){
+    this.authService.register(this.email, this.password, this.username).subscribe(
+      {
+        next: (response: {token: string}) => {
+          localStorage.setItem('token',response.token);
+          this.router.navigate(['/articles']);
+        },
+        error: (err: HttpErrorResponse) => {
+          this.errorMsg = err.error.message;
+        }
+      }
+    )
+  }
 
 }
