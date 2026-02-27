28b750a36451379c7d7687c609690803da74e0a7
Loïc Sadou <loicsadou@Host-003.lan>
2026-02-27 14:51:11 +0100

    [front] création des components de base: les pages, les services, les interfaces et un component header

---
+++ front/src/app/interfaces/article.ts
@@ -0,0 +1,10 @@
+export interface Article {
+    id: number;
+    title: string;
+    content: string;
+    authorId: number;
+    authorName: string;
+    themeId: number;
+    themeName: string;
+    createdAt: string;
+}
---
+++ front/src/app/pages/article-create/article-create.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-create',
+  imports: [],
+  templateUrl: './article-create.html',
+  styleUrl: './article-create.scss',
+})
+export class ArticleCreate {
+
+}
