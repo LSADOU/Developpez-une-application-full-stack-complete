fa16e3894b956b9855b0cf42b7fadf0564d01f38
Loïc Sadou <loicsadou@Mini-de-Loic.lan>
2026-03-15 15:27:21 +0100

    [back] ajout des entités Post et Comment

---
+++ back/src/main/java/com/openclassrooms/mddapi/entity/Comment.java
@@ -0,0 +1,41 @@
+package com.openclassrooms.mddapi.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
---
+++ back/src/main/java/com/openclassrooms/mddapi/entity/Post.java
@@ -0,0 +1,44 @@
+package com.openclassrooms.mddapi.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_ic")
+    private Topic topic;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+}
