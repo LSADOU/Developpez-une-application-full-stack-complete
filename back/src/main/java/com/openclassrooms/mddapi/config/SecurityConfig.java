a6b0f4574a2a52a0fc0370faaf99840601ea5e63
Loïc Sadou <loicsadou@Mac-mini-de-Loic.local>
2026-03-21 02:30:12 +0100

    [back] ajout d'une collection postman pour tester les endpoints

--- back/src/main/java/com/openclassrooms/mddapi/config/SecurityConfig.java
+++ back/src/main/java/com/openclassrooms/mddapi/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.mddapi.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
